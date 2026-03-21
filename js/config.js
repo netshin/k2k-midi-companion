a539be57d32e14c1ece15be7faaab6de9f0e92a5
Shay Rakovski <netshin@gmail.com>
2026-03-21 23:58:25 +0200

    Rework K2500 KDFX versioned data

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
   appName: "K2k-MIDI-Companion",
-  appVersion: "1.1.12",
+  appVersion: "1.1.13",
   modelsIndexPath: "models/index.json",
   selectedModelKey: "kurzweil-forte",
   manufacturer: "Kurzweil",
